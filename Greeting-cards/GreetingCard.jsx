950b563b280348b92e5a5e81b07fd6aac3c0e19f
deuce-szn <bryananaafi08@gmail.com>
2024-11-25 13:26:51 +0000

    Add files via upload

---
+++ Greeting-cards/GreetingCard.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line react/prop-types
+const GreetingCard = ({ title, message }) => {
+  return (
+    <div className="bg-white border border-gray-300 rounded-lg p-6 shadow-lg hover:shadow-xl transform hover:scale-105 transition-all duration-300 m-4 w-80">
+      <h2 className="text-2xl font-bold text-gray-800 mb-4">{title}</h2>
+      <p className="text-gray-600">{message}</p>
+    </div>
+  );
+};
+
+export default GreetingCard;
+
